c5d2c7e7ebc9d6cc68f0337bc0c9199634f934a8
olsonadr <olsonnick10@yahoo.com>
2019-12-05 17:30:50 -0800

    Fixed script, escaped all text, added titles to sv and do files, they are no longer figures

--- utils/file_listing.py
+++ utils/file_listing.py
@@ -7,6 +7,7 @@ import sys
 from pathlib import Path
 import posixpath
 import json
+import re
 
 def list_files(functional_dir, extension, output_file):
     # Create output file
@@ -50,23 +51,41 @@ def list_files(functional_dir, extension, output_file):
             for item in functional_unit_dir.iterdir():
                 if item.name == functional_unit_dir.name+'.'+extension:
                     with open(posixpath.join(functional_unit_dir, item.name), 'r') as f:
-                        output.write('\\begin{figure}[h]\n\\lstinputlisting['+style_text+']{'+rel_dir+'/'+functional_unit_dir.name+'/'+item.name+'}\n\\subcaption{'+caption_text+' code for the '+fu_name_escaped+' block used in the design.}\n\\end{figure}\n\n\\clearpage\n\n')
+                        output.write(fu_name_escaped+'.'+extension+'\n\\lstinputlisting['+style_text+']{'+rel_dir+'/'+functional_unit_dir.name+'/'+item.name+'}\n\n')
             overall_first = True
             for item in functional_unit_dir.iterdir():
                 if not item.name == functional_unit_dir.name+'.'+extension:
                     if item.is_file() and item.name.endswith('.' + extension):
                         with open(posixpath.join(functional_unit_dir, item.name), 'r') as f:
-                            output.write('\\begin{figure}[h]\n\\lstinputlisting['+style_text+']{'+rel_dir+'/'+functional_unit_dir.name+'/'+item.name+'}\n\\subcaption{'+caption_text+' code for the '+fu_name_escaped+' block used in the design.}\n\\end{figure}\n\n')
+                            # Escaped name of functional unit
+                            item_name_escaped = item.name
+                            i = 0
+                            while i < len(item_name_escaped):
+                                if item_name_escaped[i] == '_':
+                                    item_name_escaped = item_name_escaped[:i] + '\\' + item_name_escaped[i:]
+                                    i = i + 1
+                                i = i + 1
+
+                            output.write(item_name_escaped+'\n\\lstinputlisting['+style_text+']{'+rel_dir+'/'+functional_unit_dir.name+'/'+item.name+'}\n\n')
                     elif not item.is_file():
                         first = True
                         overall_first = False
                         for individual_block in item.iterdir():
+                            # Escaped name of individual block
+                            ind_name_escaped = individual_block.name
+                            i = 0
+                            while i < len(ind_name_escaped):
+                                if ind_name_escaped[i] == '_':
+                                    ind_name_escaped = ind_name_escaped[:i] + '\\' + ind_name_escaped[i:]
+                                    i = i + 1
+                                i = i + 1
+
                             if individual_block.is_file() and individual_block.name.endswith('.' + extension):
                                 if first:
                                     first = False
                                     output.write('\\subsubsection{Component Blocks of ' + fu_name_escaped + ' Unit}\n')
                                 with open(posixpath.join(functional_unit_dir, item.name, individual_block.name), 'r') as f:
-                                    output.write('\\begin{figure}[h]\n\\lstinputlisting['+style_text+']{'+rel_dir+'/'+functional_unit_dir.name+'/'+item.name+'/'+individual_block.name+'}\n\\subcaption{'+caption_text+' code for the '+fu_name_escaped+' block used in the design.}\n\\end{figure}\n\n')
+                                    output.write(ind_name_escaped+'\n\\lstinputlisting['+style_text+']{'+rel_dir+'/'+functional_unit_dir.name+'/'+item.name+'/'+individual_block.name+'}\n\n')
             if not overall_first:
                 output.write('\\clearpage\n\n')
 
@@ -97,33 +116,60 @@ def list_files(functional_dir, extension, output_file):
             input_text  = json_data['input_text']
             output_text = json_data['output_text']
 
+            # Escape intro_text
+            intro_text_escaped = intro_text
+            i = 0
+            while i < len(intro_text_escaped):
+                if intro_text_escaped[i] == '_':
+                    intro_text_escaped = intro_text_escaped[:i] + '\\' + intro_text_escaped[i:]
+                    i = i + 1
+                i = i + 1
+
+            # Escape input_text
+            input_text_escaped = input_text
+            i = 0
+            while i < len(input_text_escaped):
+                if input_text_escaped[i] == '_':
+                    input_text_escaped = input_text_escaped[:i] + '\\' + input_text_escaped[i:]
+                    i = i + 1
+                i = i + 1
+
+            # Escape output_text
+            output_text_escaped = output_text
+            i = 0
+            while i < len(output_text_escaped):
+                if output_text_escaped[i] == '_':
+                    output_text_escaped = output_text_escaped[:i] + '\\' + output_text_escaped[i:]
+                    i = i + 1
+                i = i + 1
+
             # Strings to write
             intro_to_write = ''
             body_to_write = ''
 
             # Subsection and intro text
-            intro_to_write += '\\catcode`\_=13\n\\def_{\textunderscore}\n\\subsection{' + fu_name_escaped + ' Functional Unit}\n\\catcode`_=8\n' + intro_text
+            intro_to_write += '\\catcode`\_=13\n\\def_{\\textunderscore}\n\\subsection{' + fu_name_escaped + ' Functional Unit}\n\\catcode`_=8\n' + intro_text_escaped
 
             # if block image exists
             for item in functional_unit_dir.iterdir():
                 if item.is_file() and item.name == fu_name + '_block.' + extension:
                     intro_to_write += ' A block diagram follows in \\textbf{Figure ' + str(fig_num) + '}. '
                     body_to_write += '\\begin{figure}[h]\n\\centering\n\\includegraphics[width=.8\\textwidth]{' + posixpath.join(rel_dir, fu_name, (fu_name + '_block.' + extension)) + '}\n\\caption{The logic design of the ' + fu_name_escaped + ' functional unit used in the final design.}\n\\end{figure}\n'
-                    fig_num + 1
+                    fig_num = fig_num + 1
             # if symbol image exists
             for item in functional_unit_dir.iterdir():
                 if item.is_file() and item.name == fu_name + '_symbol.' + extension:
                     intro_to_write += ' The logic symbol follows in \\textbf{Figure ' + str(fig_num) + '}. '
                     body_to_write += '\\begin{figure}[h]\n\\centering\n\\includegraphics[width=.48\\textwidth]{' + posixpath.join(rel_dir, fu_name, (fu_name + '_symbol.' + extension)) + '}\n\\caption{The block symbol of the ' + fu_name_escaped + ' functional unit used in the final design.}\n\\end{figure}\n'
-                    fig_num + 1
+                    fig_num = fig_num + 1
             # if sim exists
             for item in functional_unit_dir.iterdir():
                 if item.is_file() and item.name == fu_name + '_sim.' + extension:
                     intro_to_write += ' The simulation results follow in \\textbf{Figure ' + str(fig_num) + '}. '
                     body_to_write += '\\begin{figure}[h]\n\\centering\n\\includegraphics[width=.98\\textwidth]{' + posixpath.join(rel_dir, fu_name, (fu_name + '_sim.' + extension)) + '}\n\\caption{The simulation results for the ' + fu_name_escaped + ' module.}\n\\end{figure}\n'
-                    fig_num + 1
+                    fig_num = fig_num + 1
             # write functional unit stuff
-            output.write(intro_to_write + '\n\\begin{itemize}\n\\item \\textbf{Inputs:  } ' + input_text + '\n\\item \\textbf{Outputs: } ' + output_text + '\n\\end{itemize}\n' + body_to_write + '\\clearpage\n\n')
+            output.write(intro_to_write + '\n\\begin{itemize}\n\\item \\textbf{Inputs:  } ' + input_text_escaped + '\n\\item \\textbf{Outputs: } ' + output_text_escaped + '\n\\end{itemize}\n' + body_to_write + '\\clearpage\n\n')
 
 
             # begin individual block stuff
@@ -147,33 +193,60 @@ def list_files(functional_dir, extension, output_file):
                 input_text  = ind_block_json['input_text']
                 output_text = ind_block_json['output_text']
 
+                # Escape intro_text
+                intro_text_escaped = intro_text
+                i = 0
+                while i < len(intro_text_escaped):
+                    if intro_text_escaped[i] == '_':
+                        intro_text_escaped = intro_text_escaped[:i] + '\\' + intro_text_escaped[i:]
+                        i = i + 1
+                    i = i + 1
+
+                # Escape input_text
+                input_text_escaped = input_text
+                i = 0
+                while i < len(input_text_escaped):
+                    if input_text_escaped[i] == '_':
+                        input_text_escaped = input_text_escaped[:i] + '\\' + input_text_escaped[i:]
+                        i = i + 1
+                    i = i + 1
+
+                # Escape output_text
+                output_text_escaped = output_text
+                i = 0
+                while i < len(output_text_escaped):
+                    if output_text_escaped[i] == '_':
+                        output_text_escaped = output_text_escaped[:i] + '\\' + output_text_escaped[i:]
+                        i = i + 1
+                    i = i + 1
+
                 # Strings to write
                 intro_to_write = ''
                 body_to_write = ''
 
                 # Subsubsection and intro text
-                intro_to_write += '\\catcode`\_=13\n\\def_{\textunderscore}\n\\subsubsection{' + ind_name_escaped + ' Component Block}\n\\catcode`_=8\n' + intro_text
+                intro_to_write += '\\catcode`\_=13\n\\def_{\\textunderscore}\n\\subsubsection{' + ind_name_escaped + ' Component Block}\n\\catcode`_=8\n' + intro_text_escaped
 
                 # if block image exists
                 for item in Path(posixpath.join(functional_unit_dir, 'individual_blocks')).iterdir():
                     if item.is_file() and item.name == ind_name + '_block.' + extension:
-                        intro_to_write += ' A block diagram follows in \\textbf{Figure ' + str(fig_num) + '}. '
+                        intro_to_write += ' A block diagram follows in \textbf{Figure ' + str(fig_num) + '}. '
                         body_to_write += '\\begin{figure}[h]\n\\centering\n\\includegraphics[width=.8\\textwidth]{' + posixpath.join(rel_dir, fu_name, 'individual_blocks', (ind_name + '_block.' + extension)) + '}\n\\caption{The logic design of the ' + ind_name_escaped + ' module used in the ' + fu_name_escaped + ' functional unit.}\n\\end{figure}\n'
-                        fig_num + 1
+                        fig_num = fig_num + 1
                 # if symbol image exists
                 for item in Path(posixpath.join(functional_unit_dir, 'individual_blocks')).iterdir():
                     if item.is_file() and item.name == ind_name + '_symbol.' + extension:
                         intro_to_write += ' The logic symbol follows in \\textbf{Figure ' + str(fig_num) + '}. '
                         body_to_write += '\\begin{figure}[h]\n\\centering\n\\includegraphics[width=.48\\textwidth]{' + posixpath.join(rel_dir, fu_name, 'individual_blocks', (ind_name + '_symbol.' + extension)) + '}\n\\caption{The block symbol of the ' + ind_name_escaped + ' module used in the ' + fu_name_escaped + ' functional unit.}\n\\end{figure}\n'
-                        fig_num + 1
+                        fig_num = fig_num + 1
                 # if sim exists
                 for item in Path(posixpath.join(functional_unit_dir, 'individual_blocks')).iterdir():
                     if item.is_file() and item.name == ind_name + '_sim.' + extension:
                         intro_to_write += ' The simulation results follow in \\textbf{Figure ' + str(fig_num) + '}. '
                         body_to_write += '\\begin{figure}[h]\n\\centering\n\\includegraphics[width=.98\\textwidth]{' + posixpath.join(rel_dir, fu_name, 'individual_blocks', (ind_name + '_sim.' + extension)) + '}\n\\caption{The simulation results for the ' + ind_name_escaped + ' module used in the ' + fu_name_escaped + ' functional unit.}\n\\end{figure}\n'
-                        fig_num + 1
+                        fig_num = fig_num + 1
                 # write functional unit stuff
-                output.write(intro_to_write + '\n\\begin{itemize}\n\\item \\textbf{Inputs:  } ' + input_text + '\n\\item \\textbf{Outputs: } ' + output_text + '\n\\end{itemize}\n' + body_to_write + '\\clearpage\n\n')
+                output.write(intro_to_write + '\n\\begin{itemize}\n\\item \\textbf{Inputs:  } ' + input_text_escaped + '\n\\item \\textbf{Outputs: } ' + output_text_escaped + '\n\\end{itemize}\n' + body_to_write + '\\clearpage\n\n')
 
 
 if len(sys.argv) == 3:
